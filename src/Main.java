f9d9ab473d5fd50fc21f26724d31cf889b13984d
Anthon Hertz Bie <anthon_hb@hotmail.com>
2022-09-19 14:51:15 +0200

    Init

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        double a, b, x0, y0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Indsæt nu din a og b for linjens ligning:");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println("skriv dit x coordinat, tryk enter og derefter, dit x koordinat");
+        x0 = sc.nextInt();
+        y0 = sc.nextInt();
+        double distance = (Math.abs(a*x0-(b+y0)))/(Math.sqrt(1+Math.pow(a, 2)));
+        System.out.println(distance);
+
+
+    }
+
+}
